8ba8373400aec4de8ecfcad0a207cd2ccc2fdc3d
trey-sumida <54917302+trey-sumida@users.noreply.github.com>
2020-11-03 13:02:53 -1000

    replaced securityconfig with oauth config

--- clio-api/src/main/java/bytecryb/clio/config/SecurityConfiguration.java
+++ clio-api/src/main/java/bytecryb/clio/config/SecurityConfiguration.java
@@ -1,16 +1,21 @@
 package bytecryb.clio.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableOAuth2Sso
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	   @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http
+	            .authorizeRequests()
+	            .antMatchers("/", "/error**").permitAll()
+                    .anyRequest().authenticated()
+                    .and().logout().logoutUrl("/logout")
+		    .logoutSuccessUrl("/");
 
-	// Temporarily Disable Security
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.authorizeRequests().antMatchers("/").permitAll();
-	}
-
-}
+	    }
+} 
