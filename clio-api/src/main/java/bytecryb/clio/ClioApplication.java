ff8f04949b7d82b26a108b2c9067d16bef1cdc0c
Gum San Aung <aunggum@hawaii.edu>
2020-11-02 18:53:59 -1000

    inital Spring App

---
+++ clio-api/src/main/java/bytecryb/clio/ClioApplication.java
@@ -0,0 +1,13 @@
+package bytecryb.clio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClioApplication.class, args);
+	}
+
+}
