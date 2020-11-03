faf2e4bb23e01ad2e3148fa8094aa4f1efce9fdc
Gum San Aung <aunggum@hawaii.edu>
2020-11-02 21:59:15 -1000

    initial scoreboard end point

---
+++ clio-api/src/main/java/bytecryb/clio/controller/ScoreController.java
@@ -0,0 +1,45 @@
+package bytecryb.clio.controller;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import bytecryb.clio.model.ResultScore;
+import bytecryb.clio.model.Score;
+import bytecryb.clio.repository.ScoreRepository;
+
+@RestController
+@RequestMapping("/api/scores")
+public class ScoreController {
+
+	@Autowired
+	private ScoreRepository scoreRepo;
+
+	// GET weekly top scores
+
+	// GET monthly top scores
+
+	// GET yearly top scores
+
+	@GetMapping("weekly")
+	public ResponseEntity<List<ResultScore>> getWeeklyTopScores() {
+		List<Score> query = this.scoreRepo.findAll();
+
+		List<ResultScore> result = new ArrayList<ResultScore>();
+
+		Iterator<Score> scoreIterator = query.iterator();
+
+		while (scoreIterator.hasNext()) {
+			Score tmp = scoreIterator.next();
+			result.add(new ResultScore(1, "fake", tmp.getScore()));
+		}
+
+		return ResponseEntity.ok().body(result);
+	}
+}
---
+++ clio-api/src/main/java/bytecryb/clio/model/Score.java
@@ -0,0 +1,97 @@
+package bytecryb.clio.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "scores")
+public class Score {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "score_id_generator")
+	@SequenceGenerator(name = "score_id_generator", sequenceName = "score_req")
+	@Column(name = "id", updatable = false, nullable = false)
+	private long id;
+
+	@Column(name = "user_id")
+	private long userId;
+
+	@Column(name = "day")
+	private int day;
+
+	@Column(name = "month")
+	private int month;
+
+	@Column(name = "year")
+	private int year;
+
+	@Column(name = "score")
+	private int score;
+
+	public Score() {
+		super();
+	}
+
+	public Score(long userId, int day, int month, int year, int score) {
+		super();
+		this.userId = userId;
+		this.day = day;
+		this.month = month;
+		this.year = year;
+		this.score = score;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+}
