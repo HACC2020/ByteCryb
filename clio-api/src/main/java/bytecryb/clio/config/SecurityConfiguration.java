d2b007e3849aa296afb7c17321570cdde8c6a947
trey-sumida <tasumida@hawaii.edu>
2020-11-03 20:54:25 -1000

    Reformatted

--- clio-api/src/main/java/bytecryb/clio/config/SecurityConfiguration.java
+++ clio-api/src/main/java/bytecryb/clio/config/SecurityConfiguration.java
@@ -11,11 +11,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	   @Override
 	    protected void configure(HttpSecurity http) throws Exception {
 	        http
-	            .authorizeRequests()
-	            .antMatchers("/", "/error**").permitAll()
-                    .anyRequest().authenticated()
-                    .and().logout().logoutUrl("/logout")
-		    .logoutSuccessUrl("/");
-
+	        .authorizeRequests()
+			.antMatchers("/", "/error**")
+			.permitAll()
+            .anyRequest().authenticated();
 	    }
 } 
