3c0341553e8a8c264826b2ec587bc352972f7da3
Gum San Aung <aunggum@hawaii.edu>
2020-11-07 09:10:08 -1000

    created PDF model

---
+++ clio-api/src/main/java/bytecryb/clio/model/PDF.java
@@ -0,0 +1,59 @@
+package bytecryb.clio.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pdf")
+public class PDF {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pdf_id_generator")
+	@SequenceGenerator(name = "pdf_id_generator", sequenceName = "pdf_id_seq")
+	@Column(name = "id", updatable = false, nullable = false)
+	private long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Lob
+	@Column(name = "data")
+	private byte[] data;
+
+	public PDF() {
+		super();
+	}
+
+	public PDF(String name, byte[] data) {
+		super();
+		this.name = name;
+		this.data = data;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public byte[] getData() {
+		return data;
+	}
+
+	public void setPdf(byte[] data) {
+		this.data = data;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
