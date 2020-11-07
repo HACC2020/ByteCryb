ece4b45bec91212ed8cef4ac406f95ae46c46072
Gum San Aung <aunggum@hawaii.edu>
2020-11-07 09:14:59 -1000

    disabled cors and csrf

--- clio-api/src/main/java/bytecryb/clio/config/SecurityConfiguration.java
+++ clio-api/src/main/java/bytecryb/clio/config/SecurityConfiguration.java
@@ -11,6 +11,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+		httpSecurity.cors().and().csrf().disable();
 	}
 
 }
