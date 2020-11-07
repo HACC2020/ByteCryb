bd4cfd694c08e3fe396dfbfdc5cd26078db833e0
Gum San Aung <aunggum@hawaii.edu>
2020-11-06 17:32:43 -1000

    created Category model

---
+++ clio-api/src/main/java/bytecryb/clio/model/Category.java
@@ -0,0 +1,41 @@
+package bytecryb.clio.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name =  "categories")
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cat_id_generator")
+    @SequenceGenerator(name="cat_id_generator", sequenceName = "cat_seq")
+    @Column(name = "id", updatable = false, nullable = false)
+	private long id;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	public Category(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+	
+}
