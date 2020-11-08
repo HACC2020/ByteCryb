a278c683513a3cc215415c3861ed856deaec7fed
trey-sumida <54917302+trey-sumida@users.noreply.github.com>
2020-11-07 23:47:38 -1000

    roles initialized at start

---
+++ clio-api/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO roles (id, role_name) VALUES (1, 'rookie');
+INSERT INTO roles (id, role_name) VALUES (2, 'veteran');
+INSERT INTO roles (id, role_name) VALUES (3, 'proofer');
+INSERT INTO roles (id, role_name) VALUES (4, 'archivist');
