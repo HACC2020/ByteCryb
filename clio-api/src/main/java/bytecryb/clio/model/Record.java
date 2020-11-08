ad552c67f45fce15c656c5ee8ae2653345b826a2
Gum San Aung <aunggum@hawaii.edu>
2020-11-08 10:48:37 -1000

    integrate lombok

--- clio-api/src/main/java/bytecryb/clio/model/Record.java
+++ clio-api/src/main/java/bytecryb/clio/model/Record.java
@@ -8,66 +8,85 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@EqualsAndHashCode
+@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
 @Table(name = "records")
 public class Record {
 
     @Id
+    @Getter
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "record_id_generator")
     @SequenceGenerator(name = "record_id_generator", sequenceName = "record_seq")
     @Column(name = "id", updatable = false, nullable = false)
     private long id;
 
-    @Column(name = "job_id")
+    @Getter
+    @Setter
+    @Column(name = "job_id", nullable = false)
     private long jobId;
 
-    @Column(name = "status")
-    private int status;
+    @Getter
+    @Setter
+    @Column(name = "pdf_id", nullable = false)
+    private long pdfId;
 
-    @Column(name = "pdf_id")
-    private String pdfLink;
+    @Setter
+    @Column(name = "checked_out", nullable = false)
+    private boolean checkedOut;
+
+    @Setter
+    @Column(name = "submitted", nullable = false)
+    private boolean submitted;
+
+    @Setter
+    @Column(name = "approved", nullable = false)
+    private boolean approved;
+
+    @Getter
+    @Setter
+    @Type(type = "jsonb")
+    @Column(name = "json", columnDefinition = "jsonb")
+    private String json;
 
     public Record() {
         super();
+        this.jobId = -1;
+        this.pdfId = -1;
+        this.checkedOut = false;
+        this.submitted = false;
+        this.approved = false;
+        this.json = "";
     }
 
-    public Record(long jobId, int status, String pdfLink) {
+    public Record(long jobId, long pdfId, boolean checkedOut, boolean submitted, boolean approved, String json) {
         super();
         this.jobId = jobId;
-        this.status = status;
-        this.pdfLink = pdfLink;
+        this.pdfId = pdfId;
+        this.checkedOut = checkedOut;
+        this.submitted = submitted;
+        this.approved = approved;
+        this.json = json;
     }
 
-    public long getId() {
-        return id;
+    public boolean isCheckedOut() {
+        return this.checkedOut;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public boolean isSubmitted() {
+        return this.submitted;
     }
 
-    public long getJobId() {
-        return jobId;
+    public boolean isApproved() {
+        return this.approved;
     }
-
-    public void setJobId(long jobId) {
-        this.jobId = jobId;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getPdfLink() {
-        return pdfLink;
-    }
-
-    public void setPdfLink(String pdfLink) {
-        this.pdfLink = pdfLink;
-    }
-
 }
