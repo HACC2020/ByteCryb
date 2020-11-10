9b8b11a6d4bf972b0a21215d773b5f8e750e6a93
trey-sumida <54917302+trey-sumida@users.noreply.github.com>
2020-11-09 17:36:10 -1000

    removed unused import

--- clio-api/src/main/java/bytecryb/clio/controller/ScoreController.java
+++ clio-api/src/main/java/bytecryb/clio/controller/ScoreController.java
@@ -3,7 +3,6 @@ package bytecryb.clio.controller;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
