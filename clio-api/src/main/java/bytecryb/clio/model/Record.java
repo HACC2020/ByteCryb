e72e3c1d2b64361d5b1486660d57bc694ae194af
trey-sumida <54917302+trey-sumida@users.noreply.github.com>
2020-11-09 22:37:26 -1000

    removed unused imports

--- clio-api/src/main/java/bytecryb/clio/model/Record.java
+++ clio-api/src/main/java/bytecryb/clio/model/Record.java
@@ -1,15 +1,10 @@
 package bytecryb.clio.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
