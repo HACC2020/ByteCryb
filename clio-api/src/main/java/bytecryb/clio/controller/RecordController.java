e1378a108da4098eab76431ba675b8a2abada7c5
Gum San Aung <aunggum@hawaii.edu>
2020-11-09 20:08:43 -1000

    change from path-var to req-param

--- clio-api/src/main/java/bytecryb/clio/controller/RecordController.java
+++ clio-api/src/main/java/bytecryb/clio/controller/RecordController.java
@@ -4,18 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import bytecryb.clio.repository.RecordRepository;
 import bytecryb.clio.exception.ResourceNotFoundException;
 import bytecryb.clio.model.Record;
+import bytecryb.clio.repository.RecordRepository;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -24,22 +24,22 @@ public class RecordController {
     private RecordRepository recordRepo;
 
     // get all records
-    @GetMapping("/records")
+    @GetMapping("/records/all")
     public List<Record> getAll() {
         return this.recordRepo.findAll();
     }
 
-    @GetMapping("/records/{id}")
-    public ResponseEntity<Record> getById(@PathVariable(name = "id") Long id) throws ResourceNotFoundException {
+    @GetMapping("/records")
+    public ResponseEntity<Record> getById(@RequestParam(name = "id") Long id) throws ResourceNotFoundException {
         Record result = this.recordRepo.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Record" + id + "was not found"));
         return ResponseEntity.ok().body(result);
     }
 
     // get first incomplete record pdf link
-    @GetMapping("/records/{job_id}")
+    @GetMapping("/get_record_by_job")
     @Transactional
-    public ResponseEntity<Record> popByJobId(@PathVariable(name = "job_id") Long jobId) {
+    public ResponseEntity<Record> popByJobId(@RequestParam(name = "id") Long jobId) {
         // get a list of records with matching job id
         List<Record> filteredRecords = this.recordRepo.findByJobId(jobId);
 
@@ -65,9 +65,9 @@ public class RecordController {
         return ResponseEntity.ok().body(new String("Successfully Created Record: " + result.getId()));
     }
 
-    @PutMapping("/records/{id}")
+    @PutMapping("/records")
     @Transactional
-    public ResponseEntity<Record> update(@RequestBody Record input) throws ResourceNotFoundException {
+    public ResponseEntity<Record> update(@RequestParam(name = "record_id") Long id, @RequestBody Record input) throws ResourceNotFoundException {
         Record result = this.recordRepo.findById(input.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("Record not found for id: " + input.getId()));
 
