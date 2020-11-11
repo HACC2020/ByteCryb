23418dd6b61ad38325ee4613bd550c46d121e689
Gum San Aung <aunggum@hawaii.edu>
2020-11-11 00:21:18 -1000

    remove unused imports

--- clio-api/src/main/java/bytecryb/clio/repository/ScoreRepository.java
+++ clio-api/src/main/java/bytecryb/clio/repository/ScoreRepository.java
@@ -2,10 +2,6 @@ package bytecryb.clio.repository;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
