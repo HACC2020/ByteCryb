5e4d4162abb9d9e0877b553feebb67b583299198
Jenny <jhsu2@hawaii.edu>
2020-11-10 15:40:21 -1000

    reading from database

--- clio-web/src/ui/pages/ViewProfile.jsx
+++ clio-web/src/ui/pages/ViewProfile.jsx
@@ -7,8 +7,32 @@ import {
   OverlayTrigger, Row, Col, Table, NavLink
 } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
+import AuthService from '../../api/AuthService';
 
 class ViewProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: '',
+      role: '',
+      username: '',
+      score: '',
+
+    };
+    this.Auth = new AuthService();
+  }
+
+  async componentDidMount() {
+    const token = this.Auth.getToken();
+    if (token) {
+      this.setState({ token: token });
+      this.setState({ role: sessionStorage.getItem('role') });
+      const options = {};
+      let profile = await this.Auth.fetch('/api/v1/users/profile', options)
+      this.setState({ score: profile.score });
+      this.setState({ username: profile.username });
+    }
+  }
 
   render() {
 
@@ -29,13 +53,13 @@ class ViewProfile extends React.Component {
             <Col xs={8}>
               <h4>Basic Information</h4>
               <p>
-                Name: John Smith
+                User: {this.state.username}
               </p>
               <p>
-                Role: Indexer
+                Role: {this.state.role}
               </p>
               <p>
-                Total records indexed: 143
+                Score: {this.state.score}
               </p>
               <h4> Badges </h4>
               <OverlayTrigger
