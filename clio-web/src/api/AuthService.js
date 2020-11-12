3c1caf4bab7038f49e79f34d3d90602f3b4d6e95
Arslan Rakhmankulov <31532955+arslan-r@users.noreply.github.com>
2020-11-12 14:45:51 -0800

    testing preflight

--- clio-web/src/api/AuthService.js
+++ clio-web/src/api/AuthService.js
@@ -15,9 +15,13 @@ export default class AuthService {
     return this.fetch('/auth/login', {
       method: 'POST',
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+        Accept: 'text/plain;charset=utf-8',
+        'Content-Type': 'text/plain;charset=utf-8',
       },
+      // headers: {
+      //   Accept: 'application/json',
+      //   'Content-Type': 'application/json',
+      // },
       body: JSON.stringify({
         username,
         password,
