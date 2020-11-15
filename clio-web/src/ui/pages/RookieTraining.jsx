379543cfd634b6141b207013b8dfb44f9dd19b98
Jenny <jhsu2@hawaii.edu>
2020-11-15 13:53:02 -1000

    checkpoint

--- clio-web/src/ui/pages/RookieTraining.jsx
+++ clio-web/src/ui/pages/RookieTraining.jsx
@@ -4,7 +4,7 @@ import { withRouter } from "react-router-dom";
 import {
   AutoForm,
   AutoField,
-  ErrorField,
+  ErrorsField,
   TextField,
   SelectField,
   SubmitField,
@@ -13,6 +13,7 @@ import {
 import { bridge as schema } from "../../api/RookieTraining";
 import { Prompt } from "react-router";
 import { useEffect, isPrompt, shouldBlockNavigation } from "react";
+import Swal from 'sweetalert2';
 
 class RookieTraining extends React.Component {
   constructor() {
@@ -29,132 +30,86 @@ class RookieTraining extends React.Component {
       this.setState({ pageNum: num });
     };
 
-    if (this.state.pageNum === 0) {
+    if (this.state.pageNum === 1) {
       return (
-        <Container>
-          <h4>Welcome</h4>
-          <p>
-            Before you get started, we have provided a tutorial to introduce you
-            to our system.
-          </p>
-          <p>In this guide you will:</p>
-          <ul>
-            <li>Be introduced on the indexing process</li>
-            <li>Be introduced to our leaderboard system</li>
-          </ul>
-          <p>
-            Upon completion of the guide, you'll be granted access to the rest
-            of the site.
-          </p>
-          <Button variant="primary" onClick={onClickNext}>
-            Press Next to Continue
-          </Button>
-        </Container>
+          <Container>
+            <h4>Welcome</h4>
+            <p>
+              Before you get started, we have provided a tutorial to introduce you
+              to our system.
+            </p>
+            <p>In this guide you will:</p>
+            <ul>
+              <li>Be introduced on the indexing process</li>
+              <li>Be introduced to our leaderboard system</li>
+            </ul>
+            <p>
+              Upon completion of the guide, you'll be granted access to the rest
+              of the site.
+            </p>
+            <Button variant="primary" onClick={onClickNext}>
+              Press Next to Continue
+            </Button>
+          </Container>
       );
     }
 
-    console.log(this.state.pageNum);
+    // console.log(this.state.pageNum);
+
+    const onSubmit = () => {
+      Swal.fire({
+        icon: 'success',
+        title: 'Submitted!',
+        text: 'Navigating to landing page...'
+      })
+    };
 
     return (
-      <Container>
-        <Accordion style={{ marginBottom: "2rem" }}>
-          <Card>
-            <Card.Header>
-              <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                Need Help? Click here
-              </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey="0">
-              <Card.Body>
-                On the left, there is the record we want to index. In the right
-                column, you can enter the correct information based on the
-                record. If you run into an error, it is because the information
-                inputted was invalid.
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
-        <Row>
-          <Col xs={6}>
-            <embed
-              src="./ChineseArrivals_1847-1870_00001.pdf"
-              width="500rem"
-              height="550rem"
-            />
-          </Col>
-          <Col xs={5}>
-            <AutoForm schema={schema} onSubmit={console.log}>
-              <h4>Test Data</h4>
-              <AutoField name="name" />
-              <AutoField name="age" />
-              <SelectField name="gender" allowedValues={["Male", "Female"]} />
-              <TextField
-                name={"residence"}
-                help={"Only A-Z characters allowed"}
+        <Container>
+          <Accordion style={{ marginBottom: "2rem" }}>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                  Need Help? Click here
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  On the left, there is the record we want to index. In the right
+                  column, you can enter the correct information based on the
+                  record. If you run into an error, it is because the information
+                  inputted was invalid.
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+          <Row>
+            <Col xs={6}>
+              <embed
+                  src="./ChineseArrivals_1847-1870_00001.pdf"
+                  width="500rem"
+                  height="550rem"
               />
-              <TextField name="dateOfArrival" placeholder={"01/23/1832"} />
-              {/*<DateField*/}
-              {/*    showInlineError*/}
-              {/*    name="dateOfArrival"*/}
-              {/*    max={new Date(1870, 1, 1)}*/}
-              {/*    min={new Date(1847, 1, 1)}/>*/}
-              <AutoField name="nameOfShip" />
-              <AutoField name="from" />
-              <Nav.Link href="/landing">
-                <Button>
-                  Submit
-                </Button>
-              </Nav.Link>
-            </AutoForm>
-            {/*<Form>*/}
-            {/*  <Form.Row>*/}
-            {/*    <Form.Group as={Col} >*/}
-            {/*      <Form.Label>Name</Form.Label>*/}
-            {/*      <Form.Control placeholder="John Foo"/>*/}
-            {/*    </Form.Group>*/}
-
-            {/*    <Form.Group as={Col}>*/}
-            {/*      <Form.Label>Age</Form.Label>*/}
-            {/*      <Form.Control placeholder="12"/>*/}
-            {/*    </Form.Group>*/}
-            {/*  </Form.Row>*/}
-            {/*  <Form.Row>*/}
-            {/*    <Form.Group as={Col}>*/}
-            {/*      <Form.Label>Gender</Form.Label>*/}
-            {/*      <Form.Control as="select" defaultValue="Female">*/}
-            {/*        <option>Female</option>*/}
-            {/*        <option>Male</option>*/}
-            {/*      </Form.Control>*/}
-            {/*    </Form.Group>*/}
-            {/*  </Form.Row>*/}
-
-            {/*  <Form.Group>*/}
-            {/*    <Form.Label>Residence</Form.Label>*/}
-            {/*    <Form.Control placeholder="United States"/>*/}
-            {/*  </Form.Group>*/}
-
-            {/*  <Form.Group>*/}
-            {/*    <Form.Label>Date of Arrival</Form.Label>*/}
-            {/*    <Form.Control placeholder="August 23 1948"/>*/}
-            {/*  </Form.Group>*/}
-
-            {/*  <Form.Group>*/}
-            {/*    <Form.Label>Name of Ship</Form.Label>*/}
-            {/*    <Form.Control placeholder="Mayflower"/>*/}
-            {/*  </Form.Group>*/}
-
-            {/*  <Form.Group>*/}
-            {/*    <Form.Label>From</Form.Label>*/}
-            {/*    <Form.Control placeholder="New York"/>*/}
-            {/*  </Form.Group>*/}
-
-            {/*  <Button variant="primary" type="submit">*/}
-            {/*    Submit*/}
-            {/*  </Button>*/}
-            {/*</Form>*/}
-          </Col>
-        </Row>
-      </Container>
+            </Col>
+            <Col xs={5}>
+              <AutoForm schema={schema} onSubmit={() => onSubmit()}>
+                <h4>Test Data</h4>
+                <AutoField name="name" help={"Only A-Z characters allowed"}/>
+                <AutoField name="age" help={"Enter a number, or 'none' if no age shows"}/>
+                <SelectField name="gender" allowedValues={["Male", "Female"]}/>
+                <TextField
+                    name={"residence"}
+                    help={"Only A-Z characters allowed"}
+                />
+                <TextField name="dateOfArrival" placeholder={"01/23/1832"}/>
+                <AutoField name="nameOfShip" help={"Only A-Z characters allowed"}/>
+                <AutoField name="from" help={"Only A-Z characters allowed"}/>
+                <ErrorsField/>
+                <SubmitField/>
+              </AutoForm>
+            </Col>
+          </Row>
+        </Container>
     );
   }
 }
