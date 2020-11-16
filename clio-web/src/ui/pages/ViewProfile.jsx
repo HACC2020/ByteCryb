d52d52ea9432c96e40bf9a6bc49f2421e4853455
Jenny <jhsu2@hawaii.edu>
2020-11-15 23:38:55 -1000

    Update profile UI wip

--- clio-web/src/ui/pages/ViewProfile.jsx
+++ clio-web/src/ui/pages/ViewProfile.jsx
@@ -4,10 +4,13 @@ import {
   Container,
   Tooltip,
   Button,
-  OverlayTrigger, Row, Col, Table, NavLink
+  OverlayTrigger, Row, Col, Table, NavLink, Badge, Modal
 } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
 import AuthService from '../../api/AuthService';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faFile } from '@fortawesome/free-solid-svg-icons'
+
 
 class ViewProfile extends React.Component {
   constructor(props) {
@@ -17,7 +20,7 @@ class ViewProfile extends React.Component {
       role: '',
       username: '',
       score: '',
-
+      showModal: false,
     };
     this.Auth = new AuthService();
   }
@@ -28,7 +31,7 @@ class ViewProfile extends React.Component {
       this.setState({ token: token });
       this.setState({ role: sessionStorage.getItem('role') });
       const options = {};
-      let profile = await this.Auth.fetch('/api/v1/users/profile', options)
+      let profile = await this.Auth.fetch('/api/v1/users/profile', options);
       this.setState({ score: profile.score });
       this.setState({ username: profile.username });
     }
@@ -41,98 +44,116 @@ class ViewProfile extends React.Component {
       width: 'auto',
     };
 
+    const iconStyle = {
+      height: '15rem',
+      width: 'auto',
+    };
+
     const renderTooltip = (props) => (
         <Tooltip id="button-tooltip" {...props}>
-          Indexed 5 records
+          Indexed 1 record
         </Tooltip>
     );
+
+    function MoreRecords(props) {
+      return (
+          <Modal
+              {...props}
+              size="lg"
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                Records Indexed
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Table striped bordered size="sm">
+                <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Category</th>
+                  <th>View Record</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Chinese Immigration</td>
+                  <td>
+                    <Button>View Record</Button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Chinese Immigration</td>
+                  <td>
+                    <Button>View Record</Button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Chinese Marriage</td>
+                  <td>
+                    <Button>View Record</Button>
+                  </td>
+                </tr>
+                </tbody>
+              </Table>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+      );
+    }
+
 
     return (
         <Container>
           <Row>
-            <Col xs={8}>
-              <h4>Basic Information</h4>
-              <p>
-                User: {this.state.username}
-              </p>
-              <p>
-                Role: {this.state.role}
-              </p>
-              <p>
-                Score: {this.state.score}
-              </p>
+            <Col xs={6}>
+              <Container align={'center'}>
+                <Image
+                    src="https://st2.depositphotos.com/4111759/12123/v/950/depositphotos_121232794-stock-illustration-male-default-placeholder-avatar-profile.jpg"
+                    roundedCircle
+                    style={iconStyle}
+                />
+                <h5>{this.state.username}</h5>
+                <p>{this.state.role}</p>
+                <p>Score: {this.state.score}</p>
+                <NavLink href={'/edit-profile'}>
+                  <Button size="sm">
+                    <FontAwesomeIcon icon={faEdit} style={{marginRight: '0.5rem'}}/>
+                    Edit Profile
+                  </Button>
+                </NavLink>
+              </Container>
+            </Col>
+            <Col xs={6}>
               <h4> Badges </h4>
               <OverlayTrigger
                   placement="top"
                   delay={{ show: 250, hide: 400 }}
                   overlay={renderTooltip}
               >
-                <Image src={'./BadgeTest.png'} style={imgStyle}/>
+                <Badge pill variant="primary" style={{marginBottom: '5rem'}}>
+                  1st index!
+                </Badge>
               </OverlayTrigger>
-              <OverlayTrigger
-                  placement="top"
-                  delay={{ show: 250, hide: 400 }}
-                  overlay={renderTooltip}
-              >
-                <Image src={'./BadgeTest.png'} style={imgStyle}/>
-              </OverlayTrigger>
-              <OverlayTrigger
-                  placement="top"
-                  delay={{ show: 250, hide: 400 }}
-                  overlay={renderTooltip}
-              >
-                <Image src={'./BadgeTest.png'} style={imgStyle}/>
-              </OverlayTrigger>
-              <OverlayTrigger
-                  placement="top"
-                  delay={{ show: 250, hide: 400 }}
-                  overlay={renderTooltip}
-              >
-                <Image src={'./BadgeTest.png'} style={imgStyle}/>
-              </OverlayTrigger>
-            </Col>
-            <Col xs={4}>
-              <NavLink href={'/edit-profile'}>
-                <Button size="lg" block>Edit Profile</Button>
-              </NavLink>
-              <NavLink href={'/my-job'}>
-                <Button size="lg" block>My Jobs</Button>
-              </NavLink>
+              <br/>
+              <hr/>
+              <Button onClick={() => this.setState({showModal: true})}>
+                <FontAwesomeIcon icon={faFile} style={{marginRight: '0.5rem'}}/>
+                View Past Records
+              </Button>
+              <MoreRecords
+                  show={this.state.showModal}
+                  onHide={() => this.setState({showModal: false})}
+              />
             </Col>
           </Row>
-          <h3 style={{marginTop: '2rem'}}>Records Indexed</h3>
-          <Table striped bordered size="sm">
-            <thead>
-            <tr>
-              <th>#</th>
-              <th>Category</th>
-              <th>View Record</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <td>1</td>
-              <td>Chinese Immigration</td>
-              <td>
-                <Button>View Record</Button>
-              </td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Chinese Immigration</td>
-              <td>
-                <Button>View Record</Button>
-              </td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Chinese Marriage</td>
-              <td>
-                <Button>View Record</Button>
-              </td>
-            </tr>
-            </tbody>
-          </Table>
         </Container>
 
     )
