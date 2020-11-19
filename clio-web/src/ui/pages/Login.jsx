d7dac5e76daed025581f21649bb0a41f70c5a14c
Arslan Rakhmankulov <31532955+arslan-r@users.noreply.github.com>
2020-11-19 07:08:20 -0800

    cleaned up login

--- clio-web/src/ui/pages/Login.jsx
+++ clio-web/src/ui/pages/Login.jsx
@@ -11,20 +11,7 @@ import {
 import { withRouter, Redirect } from "react-router-dom";
 import AuthService from "../../api/AuthService";
 
-import Avatar from "@material-ui/core/Avatar";
-
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import { blue } from "@material-ui/core/colors";
 
 class Login extends React.Component {
   constructor(props) {
@@ -145,7 +132,7 @@ class Login extends React.Component {
             }}
           >
             <h2 align={"center"} style={{ font: "caption", fontSize: "1.5em" }}>
-              Sign in
+              SIGN IN
             </h2>
             <br />
             <Form>
